6dfebe106f0123073eff3ab61e48c4bbf1cde0d5
Krishnendu <krishnendu.dutta@codeclouds.in>
2025-08-27 20:47:43 +0530

    change router

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,14 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ThemeProvider } from "@material-tailwind/react";
+import { HashRouter } from 'react-router-dom';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <HashRouter>
+        <App />
+      </HashRouter>
     </ThemeProvider>
   </StrictMode>,
 )
